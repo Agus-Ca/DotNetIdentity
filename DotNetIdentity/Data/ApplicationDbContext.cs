65c9f2e52c8425d1b9bddbfe92f28dcf7d6033bd
Agustín Casado <agu.e.casado@gmail.com>
2022-11-21 02:15:01 -0300

    ApplicationDbContext added

---
+++ DotNetIdentity/Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotNetIdentity.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+	public ApplicationDbContext(DbContextOptions options) : base(options) { }
+}
