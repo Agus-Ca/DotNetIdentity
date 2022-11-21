f9c397d966d023a0badef9dc07d607553969a685
Agustín Casado <agu.e.casado@gmail.com>
2022-11-21 16:27:13 -0300

    AccountController and Register endpoints added

---
+++ DotNetIdentity/Controllers/AccountsController.cs
@@ -0,0 +1,15 @@
+﻿using DotNetIdentity.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetIdentity.Controllers;
+
+public class AccountsController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpGet]
+    public IActionResult Register() => View(new RegisterViewModel());
+}
