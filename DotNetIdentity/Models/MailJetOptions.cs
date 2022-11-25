e89697d88b10cfe15ae42a3e0219d58a4f50e9a9
Agustín Casado <agu.e.casado@gmail.com>
2022-11-24 22:11:06 -0300

    Email sender

---
+++ DotNetIdentity/Models/MailJetOptions.cs
@@ -0,0 +1,7 @@
+﻿namespace DotNetIdentity.Models;
+
+public class MailJetOptions
+{
+    public string ApiKey { get; set; }
+    public string SecretKey { get; set; }
+}
---
+++ DotNetIdentity/Services/EmailSender.cs
@@ -0,0 +1,67 @@
+﻿using DotNetIdentity.Models;
+using Mailjet.Client;
+using Mailjet.Client.Resources;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Newtonsoft.Json.Linq;
+
+namespace DotNetIdentity.Services;
+
+public class EmailSender : IEmailSender
+{
+    private readonly IConfiguration _configuration;
+    public MailJetOptions mailJetOptions;
+
+    public EmailSender(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public async Task SendEmailAsync(string email, string subject, string htmlMessage)
+    {
+        mailJetOptions = _configuration.GetSection("MailJet").Get<MailJetOptions>();
+
+        MailjetClient client = new MailjetClient(mailJetOptions.ApiKey, mailJetOptions.SecretKey)
+        {
+            Version = ApiVersion.V3_1,
+        };
+        MailjetRequest request = new MailjetRequest
+        {
+            Resource = Send.Resource,
+        }
+        .Property(Send.Messages, new JArray 
+        {
+            new JObject 
+            {
+                {
+                    "From",
+                    new JObject 
+                    {
+                        {"Email", "agu.e.casado@gmail.com"},
+                        {"Name", "Agustín"}
+                    }
+                }, 
+                {
+                    "To",
+                    new JArray 
+                    {
+                        new JObject 
+                        {
+                            email,
+                            {"Name", "Estimado/a" }
+                        }
+                    }
+                },
+                {
+                   "Subject",
+                   subject
+                },
+                {
+                    "HTMLPart",
+                    htmlMessage
+                }
+            }
+        });
+
+        await client.PostAsync(request);
+    }
+}
